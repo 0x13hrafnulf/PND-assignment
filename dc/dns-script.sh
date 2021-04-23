c63c1e222fb07b8bd5ec82adc81a71fc59ef995f
umarbaevB <hrafnulf13@gmail.com>
2021-04-24 00:47:59 +0200

    [A1] bash script update

--- dc/dns-script.sh
+++ dc/dns-script.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/bash -e
+
+interface=$1
+event=$2
+
+echo "$interface received $event" | systemd-cat -p info -t dispatch_script
+
+if [[ $interface != "eth0" ]] && ( [[ $event != "up" ]] || [[ $event != "dhcp6-change" ]] )
+then
+  echo "$interface $event"
+  exit 0
+fi
+
 
 ip6=$(ip -o -6 addr list eth0 | awk '{print $4}' | cut -f1)
 for ip in $ip6
@@ -13,11 +25,14 @@ done
 
 mask=$(echo $ip6 | cut -d/ -f2)
 ip=$(echo $ip6 | cut -d/ -f1)
-#echo $mask
+echo $mask
 #echo $ip
 
 ip=$(sipcalc $ip | fgrep Expanded | cut -d '-' -f 2)
 #echo $ip
+
+touch -f "/etc/dnsmasq.d/dnsmasq-ipv6.more.conf"
+echo "listen-address=$ip"  > "/etc/dnsmasq.d/dnsmasq-ipv6.more.conf"
 
 prefix_len=0
 
@@ -35,8 +50,8 @@ done
 
 prefix=$(echo $prefix | rev | cut -c 4- | rev )
 #echo $prefix
-touch -f hosts.ipv6
-echo "# IPv6 <host, IP> map " > hosts.ipv6
+touch -f "/etc/hosts.ipv6"
+echo "# IPv6 <host, IP> map " > "/etc/hosts.ipv6"
 while IFS= read -r line; do
-    echo  "$prefix$line" >> hosts.ipv6
-done < EUI64-ips
+    echo  "$prefix$line" >> "/etc/hosts.ipv6"
+done < "/etc/dnsmasq.d/EUI64-ips"
